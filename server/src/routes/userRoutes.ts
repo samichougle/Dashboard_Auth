f07ce602e2aeb256d8dada658b4519b4bb48a792
Sami Chougle <samichougle.saac@gmail.com>
2025-10-05 12:20:58 +0530

    Rename send verification mail route to register email verification and adjust forgot password routes

--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -22,18 +22,14 @@ router.post("/signup", signupUserValidation, signupUser);
 router.post("/signin", signinUserValidation, signinUser);
 
 router.post(
-  "/send-verification-mail",
+  "/register-email-verification",
   sendVerificationMailValidation,
   sendVerificationMail
 );
 
-router.post("/verify-user-mail", verifyUserMail);
+router.post("/forgot-password", verifyUserMail);
 
-router.post(
-  "/verify-forgot-mail",
-  verifyForgotMailValidation,
-  verifyForgotMail
-);
+router.post("/forgot-password", verifyForgotMailValidation, verifyForgotMail);
 router.post(
   "/forgot-password",
   sendForgotPasswordMailValidation,
